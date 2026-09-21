8fb9787db0995931c6f987ee4fd973a9e68a168d
Karl Jacob Ang <karljacobang@gmail.com>
2026-09-21 12:24:31 +0800

    feat: integrate custom fonts and enhance layout with SafeAreaView

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,29 +1,35 @@
 import '@/global.css';
+import { colors } from '@constants/theme';
 import { Link } from "expo-router";
 import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text >
-        Hello NativeWind!!
-      </Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+      <View className='flex-1 bg-background p-5'>
 
-      <Link href="/onboarding" asChild>
-        <Pressable className="mt-4 rounded bg-red-500 p-4">
-          <Text className="text-white">Go to Onboarding</Text>
-        </Pressable>
-      </Link>
+        <Text className='text-7xl font-extrabold'>Home</Text>
 
-      <Link href={{
-        pathname: '/subscriptions/[id]',
-        params: { id: 'claude' }
-      }} asChild>
-        <Pressable className="mt-4 rounded bg-red-500 p-4">
-          <Text className="text-white">Go to Subscription Details</Text>
-        </Pressable>
-      </Link>
+        <Link href="/onboarding" asChild>
+          <Pressable className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
+            <Text className="text-white">Go to Onboarding</Text>
+          </Pressable>
+        </Link>
 
-    </View>
+        <Link href="/onboarding" asChild>
+          <Pressable className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
+            <Text className="text-white">Go to Sign In</Text>
+          </Pressable>
+        </Link>
+
+        <Link href="/onboarding" asChild>
+          <Pressable className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
+            <Text className="text-white">Go to Sign Up</Text>
+          </Pressable>
+        </Link>
+
+      </View>
+    </SafeAreaView>
   )
 }
